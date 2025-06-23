3f8e8c352f7af13c9d52c2fbe85d270428c3b1e7
Coffeeblack7 <ashtonalexander95@proton.me>
2025-06-23 08:25:16 -0700

    Add files via upload
    Main .css .html and .js files for the project

---
+++ script.js
@@ -0,0 +1,22 @@
+let firstname = Ashton
+let lastname = Alexander
+let mainfolder = 
+
+
+
+function greet(){
+
+
+
+
+    greet(firstname);
+
+
+    let x;
+    let y;
+
+
+    function add(x,y){
+        let sum = x + y
+        
+    }
